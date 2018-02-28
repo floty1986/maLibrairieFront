4dd85ab32de64ac8888695ffc87b1161737079db
cdi203 <cdi203@pltz901.AFPA-PARIS-DI>
2018-02-28 09:19:54 +0100

    preparation de AdresseDAO

--- src/java/accesBDD/AdresseDAO.java
+++ src/java/accesBDD/AdresseDAO.java
@@ -1,24 +1,29 @@
-
 package accesBDD;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import javax.naming.NamingException;
-
+import obj.Adresse;
 
 public class AdresseDAO implements Serializable {
+
     private MaConnexion mc;
-    
-    public AdresseDAO(MaConnexion mc) throws NamingException{
-       mc = new MaConnexion(); 
+
+    public AdresseDAO(MaConnexion mc) throws NamingException {
+        mc = new MaConnexion();
     }
 
+    public void InsertAdresse() throws SQLException {
+        String req = "INSERT INTO Adresse(typeAdresse, numVoie, typeVoie, nomVoie, complement, codePostal, ville, pays, nom, prenom, email, telephone ) VALUES(????????????)";
+        Adresse ad = null;
 
+        try (Connection cnt = mc.getConnection();
+            PreparedStatement stm = cnt.prepareStatement(req);
+            ) {
 
-
-
-
-
-
+        }
+    }
 
 }
-
