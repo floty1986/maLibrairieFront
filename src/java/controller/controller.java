89c97e1beee20295c98dabb4836a464491e3c004
flefr <flefr@DESKTOP-EJPFI5H>
2018-03-08 09:52:41 +0100

    commit flo

--- src/java/controller/controller.java
+++ src/java/controller/controller.java
@@ -168,19 +168,7 @@ public class controller extends HttpServlet {
             if (request.getParameter("doIt") != null) {
 
                 if (bLogin.check(request.getParameter("login"), request.getParameter("password"))) {
-                    
-                     Cookie z = null;
-                    if (z != null){  
-                            pageJSP = "/WEB-INF/jspLivraison.jsp";
-//                            request.setAttribute("infoClientNom", cPanier.getNom());
-//                            request.setAttribute("infoClientPrenom", cPanier.getPrenom());
-//                            
-                            
-                             session.getAttribute("monPanier");
-                            session.getAttribute("livres");
-                            
-                            }else {
-                    
+
                     pageJSP = "/WEB-INF/jspWelcome.jsp";
                     String login = bLogin.nomPrenomClient(request.getParameter("login"));
 //                    Cookie cNom = new Cookie("nom", login);
@@ -193,7 +181,7 @@ public class controller extends HttpServlet {
                     Cookie c2 = new Cookie("try", "");
                     c2.setMaxAge(0);
                     response.addCookie(c2);
-                    }
+
                 } else {
 
                     pageJSP = "/WEB-INF/jspLogin.jsp";
@@ -235,7 +223,9 @@ public class controller extends HttpServlet {
         }
 
         if (request.getParameter("modifierCl") != null) {
-            Cookie cl = getCookie(request.getCookies(), "email");
+            Cookie cl = getCookie(request.getCookies(), "email");            
+            String login = bLogin.nomPrenomClient(cl.getValue());
+            request.setAttribute("welcome", login);
             Client c = bLogin.profilClient(cl.getValue());
             bClient.modifierClient(c.getIdClient(), request.getParameter("nom"),
                     request.getParameter("prenom"), request.getParameter("genre"),
@@ -444,7 +434,7 @@ public class controller extends HttpServlet {
 
         }
 
-         if ("jspPanier".equals(section)) {
+        if ("jspPanier".equals(section)) {
 
             pageJSP = "/WEB-INF/jspPanier.jsp";
             beanPanier livres = (beanPanier) session.getAttribute("monPanier");
@@ -454,7 +444,7 @@ public class controller extends HttpServlet {
             session.setAttribute("voirPanier", pan);
         }
         if (request.getParameter("validPanier") != null || "jspLivraison".equals(section)) {
-            
+
             Cookie c01 = getCookie(request.getCookies(), "login");
 //            response.addCookie(z);
             if (c01 == null) {
@@ -487,14 +477,14 @@ public class controller extends HttpServlet {
 
                     List<Adresse> mesAdresseL = bAdresse.adresseClient(c.getIdClient(), "LIVRAISON");
                     request.setAttribute("listeAdresseL", mesAdresseL);
-                    
+
                     List<Ouvrage> lo;
-            try {
-                lo = gestionOuvrages.findOuvrages2();
-            request.setAttribute("liste", lo);
-            } catch (SQLException ex) {
-            ex.printStackTrace();
-            }
+                    try {
+                        lo = gestionOuvrages.findOuvrages2();
+                        request.setAttribute("liste", lo);
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
 
                 } catch (SQLException ex) {
                     ex.printStackTrace();
@@ -712,7 +702,7 @@ public class controller extends HttpServlet {
 
         if ("supprimerAdFact".equals(section)) {
             try {
-                
+
                 int idAdresse = Integer.valueOf(request.getParameter("idAdresse"));
                 bAdresse.supAdresse(idAdresse);
             } catch (SQLException ex) {
@@ -722,7 +712,7 @@ public class controller extends HttpServlet {
         if ("supprimerAdLiv".equals(section)) {
 
             try {
-                
+
                 int idAdresse = Integer.valueOf(request.getParameter("idAdresse"));
                 bAdresse.supAdresse(idAdresse);
             } catch (SQLException ex) {
